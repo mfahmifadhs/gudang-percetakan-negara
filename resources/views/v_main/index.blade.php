17163505d63221118dfad57b0dab0b0d0638d1b9
mfahmifadh <mfahmifadh@gmail.com>
2022-09-05 14:19:37 +0700

    gambar main page

--- resources/views/v_main/index.blade.php
+++ resources/views/v_main/index.blade.php
@@ -8,8 +8,8 @@
       <div class="row g-5">
         <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
           <div class="position-relative overflow-hidden h-100" style="min-height: 400px;">
-            <img class="position-absolute w-100 h-100 pt-5 pe-5" src="img/about-1.jpg" alt="" style="object-fit: cover;">
-            <img class="position-absolute top-0 end-0 bg-white ps-2 pb-2" src="img/about-2.jpg" alt="" style="width: 200px; height: 200px;">
+            <img class="position-absolute w-100 h-100 pt-5 pe-5" src="{{ asset('dist-main/img/gudang-pn.jpg') }}" alt="" style="object-fit: cover;">
+            <img class="position-absolute top-0 end-0 bg-white ps-2 pb-2" src="{{ asset('dist-main/img/roadmap-gudang-pn.jpg') }}" alt="" style="width: 200px; height: 200px;">
           </div>
         </div>
         <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
@@ -18,12 +18,14 @@
             <h1 class="display-6 mb-5">Gudang Percetakan Negara Kemenkes RI</h1>
             <div class="bg-light border-bottom border-5 border-primary rounded p-4 mb-4">
               <p class="text-dark mb-2">
-                Gudang Percetakan Negara Kemenkes RI merupakan Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Kompleks Pergudangan dan Perkantoran Kementerian Kesehatan yang terletak di Jl. Percetakan Negara No. 23 selesai dibangun pada tahun 1950
+              dengan total luas 69.104 m2. <br> Kompleks ini terdiri dari 12 bangunan Gedung , antara lain: <br><br>
+              1. Gedung Laboratorium Penelitian Penyakit Infeksi (Gd. Prof Dr. Sri Oemijati),<br>
+              2. Gedung Perkantoran Biomedis,<br>
+              3. Gedung Records Center Kemenkes RI (Gd. Soedjoto),<br>
+              4. Gedung Perkantoran, dan<br>
+              5. 8 Gudang Kementerian Kesehatan RI<br>
+
               </p>
             </div>
             <a class="btn btn-primary py-2 px-3 me-3" href="{{ url('unit-kerja/surat/pengajuan/penyimpanan') }}">
